65160c09d8d230e9edc7cdd9431488ca8099911f
AishwaryaPalanivel <47705869+AishwaryaPalanivel@users.noreply.github.com>
2025-03-06 08:32:42 +0530

    Add files via upload

---
+++ Stock_max_profit.java
@@ -0,0 +1,32 @@
+package leetCode;
+
+public class Stock_max_profit {
+
+	
+		public static int maxProfit(int[] prices) {
+	        if (prices == null || prices.length == 0) {
+	            return 0;
+	        }
+	        
+	        int minPrice = Integer.MAX_VALUE;
+	        int maxProfit = 0;
+	        
+	        for (int price : prices) {
+	            if (price < minPrice) {
+	                minPrice = price; // Update the minimum price
+	            } else if (price - minPrice > maxProfit) {
+	                maxProfit = price - minPrice; // Update max profit if selling at this price is better
+	            }
+	        }
+	        
+	        return maxProfit;
+	    }
+
+	    public static void main(String[] args) {
+	        int[] prices = {7, 1, 5, 3, 6, 4};
+	        System.out.println("Maximum Profit: " + maxProfit(prices));
+	    }
+
+	}
+
+
